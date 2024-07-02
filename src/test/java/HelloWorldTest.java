078bd6b1bd28baa40942e515ce28dd38b4bf433f
iren985 <iren985@gmail.com>
2024-07-03 00:27:14 +0300

    HelloWorldTest1

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.net.URI;
 
 public class HelloWorldTest {
     @Test
     public void TestHelloWorld(){
-        System.out.println("Hello World");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
