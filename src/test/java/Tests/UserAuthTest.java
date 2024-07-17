386d83df4979bb1a5d45ae701ed2d34c586c8eb6
IFinenko <iren985@gmail.com>
2024-07-17 23:56:48 +0300

    Simpletest

--- src/test/java/Tests/UserAuthTest.java
+++ src/test/java/Tests/UserAuthTest.java
@@ -1,5 +1,7 @@
+package Tests;
+
+import Lib.BaseTestCase;
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -14,7 +16,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class UserAuthTest {
+public class UserAuthTest extends BaseTestCase {
     String cookie;
     String header;
     int userIdOnAuth;
@@ -31,9 +33,9 @@ public class UserAuthTest {
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
 
-        this.cookie = responseGetAuth.getCookie("auth_sid");
-        this.header = responseGetAuth.getHeader("x-csrf-token");
-        this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+        this.cookie = this.getCookie(responseGetAuth,"auth_sid");
+        this.header = this.getHeader(responseGetAuth,"x-csrf-token");
+        this.userIdOnAuth = this.getIntFromJson(responseGetAuth,"user_id");
 
     }
     @Test
